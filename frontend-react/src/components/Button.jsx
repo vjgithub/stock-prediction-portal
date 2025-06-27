8ec133cfe4b83ebe1e934e7a9ded844d6624005f
vj <vj.diroberto@gmail.com>
2025-06-27 09:03:58 -0400

    homepage react

---
+++ frontend-react/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <>
+                <a className={`btn ${props.class}`} href="#">{props.text}</a>
+               
+
+    </>
+  )
+}
+
+export default Button
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className = 'footer py-3 my-3 text-light'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy;2025 V.J. DiRoberto</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
